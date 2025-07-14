81be7cd944f86c3f0275c1a66da7190b996d7306
suha720 <a50914513@gmail.com>
2025-07-14 10:51:37 +0900

    [docs] 유틸리티의 이해

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
-// 저장하기 관련 클래스
-class todoStorage<T> {
-  // 내부에서만 사용할 변수
-  private items: T[] = [];
-  // 메소드 만으로 즉, 검증된 과정으로만 내부 item 배열 접근
-  add(item: T): void {
-    this.items.push(item);
-  }
-  read(): T[] {
-    return this.items;
-  }
+function getScore(subject: string, score: number) {
+  console.log(`${subject}의 점수는 ${score} 입니다.`);
 }
-const result = new todoStorage<string>();
-result.add("ㅠㅠ");
-result.read();
+/**
+ * Parameters 은 함수의 매개변수 데이터 종류 추출
+ *  [subject: string, score: number]
+ */
+
+type ScoreType = Parameters<typeof getScore>;
