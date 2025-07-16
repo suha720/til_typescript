f71638b2f416a7db7f1ab24148d26e6ced987fdf
suha720 <a50914513@gmail.com>
2025-07-16 10:10:41 +0900

    [docs] 타입캐스트 as 정리

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,6 @@
-/**
- * API 요청을 한다.
- * 4가지 상태가 흔히 활용된다.
- *
- * "DONE" - 요청을 실행 상태
- * "LOADING" - 요청이 진행 중인 상태
- * "ERROR" - 요청이 실패 상태
- * "INIT" - 초기 상태
- */
-enum Status {
-  DONE = "DONE",
-  LOADING = "LOADING",
-  ERROR = "ERROR",
-  INIT = "INIT",
-}
+let numVar: any = 5;
+numVar = true;
 
-const doneStatus = "DONE";
-const LoadingStatus = "LOADING";
-const errorStatus = "ERROR";
-const initStatus = "INIT";
-
-function runNetwork() {
-  let status = Status.INIT;
-  try {
-    status = Status.LOADING;
-    // 복잡한 처리 ....
-    // 복잡한 처리 ....
-    status = Status.DONE;
-  } catch (error) {
-    status = Status.ERROR;
-  } finally {
-    return status;
-  }
-}
-
-console.log(runNetwork() === "DONE");
+// as 강제로 데이터 타입을 지정하기 위한 자리
+let temp = numVar as string;
+temp.toUpperCase();
