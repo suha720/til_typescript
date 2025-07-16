8bb21c421ec90e689b8d092e1a7cb2847096d5e6
suha720 <a50914513@gmail.com>
2025-07-16 10:02:02 +0900

    [docs] Enum 정리

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,36 @@
-// interface IdolType {
-//   name: string;
-//   age?: string; // 옵셔널
-//   year: number;
-// }
+/**
+ * API 요청을 한다.
+ * 4가지 상태가 흔히 활용된다.
+ *
+ * "DONE" - 요청을 실행 상태
+ * "LOADING" - 요청이 진행 중인 상태
+ * "ERROR" - 요청이 실패 상태
+ * "INIT" - 초기 상태
+ */
+enum Status {
+  DONE = "DONE",
+  LOADING = "LOADING",
+  ERROR = "ERROR",
+  INIT = "INIT",
+}
 
-type IdolType = {
-  readonly name: string; // 읽기전용
-  age?: string; // 옵셔널
-  year: number;
-};
+const doneStatus = "DONE";
+const LoadingStatus = "LOADING";
+const errorStatus = "ERROR";
+const initStatus = "INIT";
 
-let bts: IdolType = {
-  name: "BTS",
-  year: 2020,
-};
+function runNetwork() {
+  let status = Status.INIT;
+  try {
+    status = Status.LOADING;
+    // 복잡한 처리 ....
+    // 복잡한 처리 ....
+    status = Status.DONE;
+  } catch (error) {
+    status = Status.ERROR;
+  } finally {
+    return status;
+  }
+}
 
-bts.name = "홍성"; // Error 발생
+console.log(runNetwork() === "DONE");
