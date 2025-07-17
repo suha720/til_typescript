6ea7ef3f028230be8a53c2f0a234f299ad2ba0b4
suha720 <a50914513@gmail.com>
2025-07-17 09:45:44 +0900

    [docs] Intersection 의 이해

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
-let numVar: any = 5;
-numVar = true;
+interface Human {
+  name: string;
+  age: number;
+}
 
-// as 강제로 데이터 타입을 지정하기 위한 자리
-let temp = numVar as string;
-temp.toUpperCase();
+interface Contacts {
+  phone: string;
+  address: string;
+}
+
+type HumanContacts = Human & Contacts;
+// 반드시 모든 속성이 존재해야 한다.
+let iu: HumanContacts = {
+  address: "서울",
+  age: 28,
+  name: "아이유",
+  phone: "111",
+};
