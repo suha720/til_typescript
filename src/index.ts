c27f52af91673568cb1ad5262c3a79de1f35a43e
suha720 <a50914513@gmail.com>
2025-07-21 10:16:39 +0900

    [docs] key 와 value 편리하게 추출하기

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,13 @@
-interface Dog {
-  name: string;
-  age: number;
-}
-interface Cat {
-  name: string;
-  breed: string;
+interface LoadingState {
+  type: "loading";
+  data: string[];
 }
 
-type DogCat = Dog | Cat;
-
-function isDog(a: DogCat): a is Dog {
-  return (a as Dog).age !== undefined;
+interface ErrprState {
+  type: "error";
+  message: string[];
 }
 
-const pp: DogCat = { name: "멍멍이", age: 10 };
-const cc: DogCat = { name: "멍멍이", breed: "샴" };
-
-if (isDog(pp)) {
-  // Dog 라는 코드 진행
-  pp; // const pp: Dog
-} else {
-  // Cat 이라는 코드 진행
-}
+type FetchStatus = LoadingState | ErrprState;
+// type StatusType = "loading" | "error";
+type StatusType = FetchStatus["type"];
